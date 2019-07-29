3793773be8b92ed4e974cbb3570d31bad1c5cdee
Wills Jarquin <jarquinwills@gmail.com>
2019-07-29 17:17:42 +0000

    Fixed the photo db

--- db/migrate/20190726010830_add_picture_to_photo.rb
+++ db/migrate/20190726010830_add_picture_to_photo.rb
@@ -1,5 +1,6 @@
 class AddPictureToPhoto < ActiveRecord::Migration[5.2]
   def change
     add_column :photos, :picture, :string
+    add_column :photos, :user_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_010830) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
